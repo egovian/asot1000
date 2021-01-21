4430e23863f935208eadc7c94a2b81f00f124ced
Jaime Solsona <jaimecr9@gmail.com>
2021-01-21 16:58:34 +0100

    RSS site crashed, bypass

--- server.py
+++ server.py
@@ -36,17 +36,19 @@ def update_times():
     res = req.get('https://nitter.net/asot/rss')
 
     text = str(res.content)
-    item = text.split("<item>")[1].split('<title>')[1]
-    if item.startswith('#'):
-        number = item.split('#')[1].split(" ")[0]
-        print(number)
-        datetxt = item.split('<pubDate>')[1].split('</pubDate>')[0]
-        date = datetime.strptime(datetxt, "%a, %d %b %Y %H:%M:%S GMT")
-        update_time = {"number":number, "time":date.isoformat(), "update":"yes"}
-        return update_time
+    if len(text.split("<item>")) > 1:
+        item = text.split("<item>")[1].split('<title>')[1]
+        if item.startswith('#'):
+            number = item.split('#')[1].split(" ")[0]
+            print(number)
+            datetxt = item.split('<pubDate>')[1].split('</pubDate>')[0]
+            date = datetime.strptime(datetxt, "%a, %d %b %Y %H:%M:%S GMT")
+            update_time = {"number":number, "time":date.isoformat(), "update":"yes"}
+            return update_time
+        else:
+            return {"number":0, "time":datetime.now().isoformat(), "update":"no"}
     else:
         return {"number":0, "time":datetime.now().isoformat(), "update":"no"}
-            
 
 
 
